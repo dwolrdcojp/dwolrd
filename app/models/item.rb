9512b0b0ed397970de0da3e011100c26d6b71054
Max Schrementi <dwolrdcojp@gmail.com>
2016-07-08 18:05:26 -0500

    Add Image Management

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,21 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+  validates :user_id, :presence  => true
   validates :content, :presence  => true
+  validate  :picture_size
   validates :title,   :presence  => true,
                       :length    => { :minimum => 5 }
   has_many :comments, :dependent => :destroy
-  belongs_to :user
+
+  
+  private
+
+    # Validates the size of an uploaded picture
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
