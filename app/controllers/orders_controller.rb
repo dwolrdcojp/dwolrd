2d5d82d66f728ac7915d2ab7f953ac7cb194a398
Max Schrementi <dwolrdcojp@gmail.com>
2016-10-15 00:27:17 -0500

    WTFFFFFFF

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -46,7 +46,7 @@ class OrdersController < ApplicationController
         :description => @item.title,
         :application_fee => ((@item.price * 100) * 0.089).floor + 30
       },
-      {:stripe_account => ENV["STRIPE_CONNECT_CLIENT_ID"]}
+      {:stripe_account => ENV['STRIPE_PUBLISHABLE_KEY']}
     )
       @order.name = params[:stripeShippingName]
       @order.address = params[:stripeShippingAddressLine1]
