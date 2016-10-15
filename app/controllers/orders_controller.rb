addc2cede2e01e1a9fb8d35058add9b3b291a2f4
Max Schrementi <dwolrdcojp@gmail.com>
2016-10-15 00:50:38 -0500

    Time to go home

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,21 +32,21 @@ class OrdersController < ApplicationController
 
     begin
     
-    # customer = Stripe::Customer.create(
-    #     :email => params[:stripeEmail],
-    #     :source  => token
-    # )
+    customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => token
+    )
 
     require 'json'
 
       charge = Stripe::Charge.create({
-        :source => token,
+        :customer => customer.id,
         :amount => (@item.price * 91.1).floor - 30,
         :currency => "usd",
         :description => @item.title,
         :application_fee => ((@item.price * 100) * 0.089).floor + 30
       },
-      {:stripe_account => @item.user.publishable_key }
+      {:stripe_account => ENV["STRIPE_CONNECT_CLIENT_ID"] }
     )
       @order.name = params[:stripeShippingName]
       @order.address = params[:stripeShippingAddressLine1]
