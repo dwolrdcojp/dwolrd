e324e690e0823e4fa9dc7af554c59c9cbf490b91
Max Schrementi <dwolrdcojp@gmail.com>
2016-12-02 09:17:33 -0500

    Add Index Action To Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   rescue_from ActiveRecord::RecordInvalid, with: :deny_access
 
+  def index
+
+  end
+
   def show
     @user = User.find(params[:id])
   end
