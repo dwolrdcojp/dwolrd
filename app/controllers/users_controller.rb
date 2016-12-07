0a76a907da256fe34dcaedfb6cafffe5a9f09758
Max Schrementi <dwolrdcojp@gmail.com>
2016-12-06 20:43:07 -0500

    Change UserNotifier to User Notifier Mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       # Deliver the signup email
-      UserNotifier.send_signup_email(@user).deliver
+      UserNotifierMailer.send_signup_email(@user).deliver
       redirect_to(@user, :notice => 'User created')
     else
       render :action => 'new'
