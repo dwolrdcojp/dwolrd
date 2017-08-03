4a1e81023acb2d8f37246e7f1b9e17bea0f18df6
Max Schrementi <dwolrdcojp@gmail.com>
2017-08-03 18:13:15 -0500

    Fix order callback

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -85,7 +85,7 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @@order = Order.find(params[:id])
+      @order = Order.find(params[:id])
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
