25e7ba0a08eafa89c61e8a6f2a42eff6b1c94692
Max Schrementi <dwolrdcojp@gmail.com>
2017-08-03 16:42:51 -0500

    Add Shipping Info

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ApplicationRecord
   belongs_to :item
   belongs_to :buyer, class_name: "User"
   belongs_to :seller, class_name: "User"
+  validates :name,  :presence  => true
+  validates :address, :presence  => true
+  validates :city,  :presence  => true
+  validates :state,    :presence  => true
+  validates :zip,    :presence  => true
+  validates :country,    :presence  => true
 end
