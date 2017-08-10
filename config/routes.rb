1f663f33d9b8a2736ff00b4bf57e0d765cbee513
Max Schrementi <dwolrdcojp@gmail.com>
2017-08-10 00:27:59 -0500

    Update Rails Routes page

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   root 'items#index'
   
-  get '/about',   to: 'static_pages#about'
-  get '/garage',  to: 'items#garage'
-  post '/paypal/ipn_notify', to: 'orders#notify'
+  get 'about',   to: 'static_pages#about'
+  get 'garage',  to: 'items#garage'
+  post 'paypal/ipn_notify', to: 'orders#notify'
   get 'sales'     => "orders#sales"
   get 'purchases' => "orders#purchases"
   get 'favorites' => "items#favorites"
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
-  get '/items/new' => 'items#new', as: :user_root
+  get 'items/new' => 'items#new', as: :user_root
 
   resources   :items do
     resource :order, only: [:new, :create]
